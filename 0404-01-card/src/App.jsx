6b54c571eefd9f14e4aee34bcfeb2c4a08ed2e90
Choi-jujuyeon <chlwndus1216@naver.com>
2023-04-05 00:02:10 +0900

    포커 카드 만들기 복습(주석)

--- 0404-01-card/src/App.jsx
+++ 0404-01-card/src/App.jsx
@@ -1,13 +1,41 @@
 import Card from "./components/Card";
 import cardData from "./components/cardData.json";
 
+//포커 카드 만들기
 function App() {
     return (
         <div className="grid grid-cols-4 justify-center items-center gap-8 py-8">
+            {/*모든 카드에 적용할 디자인
+
+            => 배치
+                : grid 사용 -> 2차원 배치를 할 것이다.
+                : grid-cols-4 사용 -> 한줄에 4개씩 배치 
+                : gap -> 그리드 사이의 간격을 조절
+                : py -> padding의 top과 bottom을 조절
+
+            => 정렬
+                : justify-center -> (가로 축) 컨테이너내에서 중앙으로 정렬
+                : items-center -> 각각의 컨테이너의 크기가 다르더라도 컨테이너의 교차 중심에 따라 정렬을 해준다. */}
+
             {cardData.map((v, i) => {
                 return <Card key={i} symbol={v.symbol} score={v.score} />;
             })}
+            {/* map함수 사용
+
+            =>  map 함수란 ?
+                : 배열의 요소를 불러와 새로운 배열을 반환한다. (= 같은 기능을 반복할 때 사용)
+
+            => v(value) : 현재 값을 의미한다.
+            => i(index) : 배열 내 현재 값의 인덱스를 의미한다. 
+
+            => cardData에서 값의 symbol과 score을 가져와 새로운 배열로 return한다.
+
+
+            => key={i}
+                : react에서 제공하는 Hook이다.*/}
+
             <Card />
+            {/* 컴포넌트 사용 */}
         </div>
     );
 }
--- 0404-01-card/src/components/Card.jsx
+++ 0404-01-card/src/components/Card.jsx
@@ -1,4 +1,7 @@
+// 이 함수에서는 추출한 요소에 조건문을 달아 스타일을 적용할 것이다.
 function Card({ symbol, score }) {
+    // map함수를 통해 리턴된 symbol과 score을 Card()의 요소로 넣어준다.
+
     return (
         <div
             className={`relative w-24 h-36 flex justify-center items-center border-2 ${
@@ -7,9 +10,42 @@ function Card({ symbol, score }) {
                     : "border-red-500 text-red-500"
             }`}
         >
+            {/*className 속성
+                : HTML요소에 CSS클래스를 추가하는 데 사용한다.  
+                : 스타일을 동적으로 변경할 수 있다.
+
+            => {`string string ${변수}`} 와 같이 백틱을 사용하는 이유!!!
+                : 문자열을 사용하기 위해서. 
+                
+
+            => relative(부모) && absolute(자식)
+                : 부모의 위치를 기준점으로 삼아 자식의 배치를 결정할 수 있다.
+
+            => 배치 
+                : justify-center
+                : items-center
+            
+                : flex
+                    -> 수평 수직 영역 중 하나의 방향만 설정 가능(1차원 배치)
+                    -> 컨테이너 안에 있는 items의 방향을 기본적으로 row로 설정한다. (==카드를 만든 후 나열할 방향을 설정)
+
+                : w-24 -> width 값을 설정
+                : h-36 -> height 값을 설정
+                : border-2 -> border 값을 설정 
+                
+            => 삼항 연산자 ( 조건문 ? exprIfTrue : exprIfFalse )*/}
+
             <div className={"absolute top-0 left-0 m-1"}>{symbol}</div>
             <div>{score}</div>
             <div className={"absolute bottom-0 right-0 m-1"}>{symbol}</div>
+
+            {/* 
+            absolute : 부모의 위치를 기준으로 배치를 한다 
+                => top : 부모 기준 top 위치 설정
+                => left : 부모 기준 left 위치 설정
+                => m-1 : margin 설정
+            
+            {~~~} : javascript의 변수를 사용할때 중괄호로 감싸줘야 한다. */}
         </div>
     );
 }
